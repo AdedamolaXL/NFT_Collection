0dcecca7c9a46709a40a2db127cf9cc038fa614c
adedamolaxl <adedamolaxl@outlook.com>
2022-06-01 06:09:53 +0100

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xfb802ee16a7fadef91cf8e1aecdf8063c11f3853";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
